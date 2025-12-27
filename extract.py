ab0bd144142e3bc1042e5d2b2175aa3c72593218
wrinklykong <johnwjones1337@gmail.com>
2025-12-27 01:24:09 -0600

    Working MIDI extraction with somewhat working notes

--- extract.py
+++ extract.py
@@ -6,6 +6,8 @@ import json
 import glob
 from pathlib import Path
 
+from midiutil.MidiFile import MIDIFile
+
 ALLOWED_FILENAME_LETTERS = string.ascii_letters + string.digits + '.'
 TEST_FILE = "data/music/credits.mus"
 TITLE_HEADER = 20  # bytes reserved for the header, if it exists
@@ -17,10 +19,35 @@ SAMPLE_STRUCT_SIZE = 0x2c
 SAMPLE_STRUCT_FORMAT = "<20sIIIIII"
 SAMPLE_STRUCT = struct.Struct(SAMPLE_STRUCT_FORMAT)
 
+NOTE_SCALE = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
 
 def remove_trailing_zeros_from_str(s):
     return s.replace('\x00', '')
 
+
+def calculate_note(notes):
+    if notes == 255:
+        return 'None'
+    notes_from_c = notes // 8
+    scale_num = int(notes_from_c // len(NOTE_SCALE))
+    note = NOTE_SCALE[notes_from_c % len(NOTE_SCALE)]
+    return note + str(scale_num)
+
+
+class NoteInfo:
+
+    def __init__(self, bytes):
+        self.sample_num = bytes[0]  # First 2 bytes refer to the sample number, typically < 31
+        self.second_byte = bytes[2:4]  # number of notes after C0 * 8 (8 samples per note)
+        self.note_played = calculate_note(self.second_byte[0])
+        self.note_played_raw = self.second_byte[0] // 8 + 24
+        # C0 starts at 12 for MIDI, add 12 to it to make it "midi compliant"
+        self.note_played_midi = self.note_played_raw + 12
+        self.third_byte = bytes[4:]
+    
+    def __str__(self):
+        return f"SampleNum: {self.sample_num}, Note: {self.note_played}, ThirdByte: {self.third_byte.hex()}"
 
 class Sample:
     def __init__(self, sname, stype, ssize, siden, sloops, sloope, edata, snume):
@@ -28,19 +55,23 @@ class Sample:
         self.stype = stype
         self.ssize = ssize
         self.siden = siden
+        # NDS7 - SOUNDxPNT - Sound Channel X Loopstart Register (W)
         self.sloops = sloops
         self.sloope = sloope
         self.edata = edata
         self.snume = snume
+        # Assume its a one shot if no loop start is given (?)
+        self.type = 1 if sloops else 2
     
     def __str__(self):
         s = [f"Sample Name: {self.sname}",
              f"Type: {self.stype}",
              f"Size: {self.ssize} bytes",
-             f"Iden: {self.siden}",
+             f"Start Note: {hex(self.siden)}",
              f"Loop start: {self.sloops}",
              f"Loop end: {self.sloope}",
-             f'Section num: {self.snume}']
+             f'Section num: {self.snume}',
+             f'Type: {"one shot" if self.type == 2 else "loop"}']
         return "\n\t".join(s)
 
 
@@ -119,19 +150,46 @@ def get_seq_data(vfile):
 
 
 def display_seq_info(seq_info):
-    header = "0000|1111|2222|3333|4444|5555|6666|7777|8888|9999|AAAA|BBBB|CCCC|DDDD|EEEE|FFFF"
+    header = "0000|1111|2222|3333|4444|5555|6666|7777|8888|9999|AAAA|BBBB|CCCC|DDDD|EEEE|FFFF\n" + "-"*79
     contents = []
     for x in seq_info:
         # split into 4s to add the barrier between
         split_thing = []
         for i in range(len(x)//4):
-            split_thing.append(''.join([str(y) for y in x[i*4:(i+1)*4]]))
+            split_thing.append(''.join([str(y.sample_num) for y in x[i*4:(i+1)*4]]))
         contents.append('|'.join(split_thing))
+        for y in x:
+            print(y)
+        print('')
     print(header)
     for c in contents:
         print(c)
     print('')
 
+
+def generate_midi_tracks(seq_info):
+    # Create a new MIDI file for it ! yayy...
+    # len(seq_info) = number of channels in there
+    for c_num, channel in enumerate(seq_info):
+        mf = MIDIFile(len(seq_info))
+        track = 0
+        time = 0  # start at beginning
+        mf.addTrackName(track, time, f"MIDI_Ch_{c_num}")
+        # TODO: Get the BPM somehow too
+        mf.addTempo(track, time, 120)
+        # go through each note and add the note info to the sequence!
+        for n_index, note in enumerate(channel):
+            if note.note_played == 'None':
+                continue
+            pitch = note.note_played_raw
+            print(pitch)
+            duration = 1  # 1 beat long
+            time = n_index/4  # when the note strikes... split into quarters
+            volume = 127  # TODO: Figure out the volume stuff in a bit
+            mf.addNote(0, c_num, pitch, time, duration, volume)
+        with open(f"{c_num}.midi", "wb") as f:
+            mf.writeFile(f)
+    
 
 def get_note_info(vfile, num_channels):
 
@@ -149,12 +207,13 @@ def get_note_info(vfile, num_channels):
         # split into 6 byte parts for each thing
         for _ in range(64):
             for channel in range(num_channels):
-                note = notes_file.read(6)
+                note_obj = NoteInfo(notes_file.read(6))
                 if seq_info[channel] == None:
-                    seq_info[channel] = [note[0]]
+                    seq_info[channel] = [note_obj]
                 else:
-                    seq_info[channel].append(note[0])
+                    seq_info[channel].append(note_obj)
         display_seq_info(seq_info)
+    return seq_info
 
 
 def process_mus_file(filename):
@@ -170,45 +229,14 @@ def process_mus_file(filename):
     # parse the seq header from 0x568 - 0x774
     num_channels, order = get_seq_data(VF)
     # Now we get into the track information... 0x774 til end
-    get_note_info(VF, num_channels)
+    seq_info = get_note_info(VF, num_channels)
+    generate_midi_tracks(seq_info)
     # calculate the end
     len_tracks = VF.size - 0x774
     section_size = 6 * num_channels * 64
     return len_tracks, num_samples, order
 
 def main():
-    # read all files that end with .mus, get the size of the data and get some info on it...
-    # with open(TEST_FILE, "rb") as f:
-    #     read_data = f.read()
-    # 
-    # VF = VirtualFile(read_data)
-    # # Parse the sample header from 0x0 - 0x568
-    # samples = get_sample_data(VF)
-    # num_samples = len(samples)
-    # # parse the seq header from 0x568 - 0x774
-    # order = get_seq_data(VF)
-    # num_seq = len(order)
-    # num_sections = max(order) + 1
-    # print(f'Num seq: {num_seq}; order: {order}, num_sections: {num_sections}')
-    # # Now we get into the track information... 0x774 til end
-    # # calculate the end
-    # len_tracks = VF.size - 0x774
-    # print(len_tracks)
-    # # 6 is assumed note info struct length
-    # num_notes = len_tracks / 6
-    # print(f'{num_notes=}')
-    # # there are EVENLY divided sections
-    # size_of_section = num_notes / num_sections
-    # print(f'{size_of_section=}')
-    # do some research on all of the bytes n stuff
-    # ret = get_note_info(VF)
-    # a = set()
-    # for r in ret:
-    #     if r[0] == 1:
-    #         a.add(hex(r[2] & ~0x0F))
-    # print(a)
-    # print([hex(a) for a in ret if a[0] == 5])
-    # print(json.dumps(thing, indent=4))
     data = {}
     for mfile in glob.glob("data/music/pokerintro.mus"):
         filename = Path(mfile).name
@@ -223,7 +251,7 @@ def main():
             "NumSamples": nsample,
             "NumChannels": seqs[0],
             "NumSequencesPlayed": seqs[1],
-            # "Seqs": seqs[2:],
+            "Seqs": seqs[2:],
             "CalcNumSeqs": tracklen/section_size
         }})
     print(json.dumps(data, indent=4))
